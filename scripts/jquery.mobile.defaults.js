745818375b8540ea3e0920c0c5356739f5a62f0b
Edgardo Jimenez <edgardo.jimenez@live.com>
2013-08-01 16:38:32 -0500

    Fixed appCache and build file

--- scripts/jquery.mobile.defaults.js
+++ scripts/jquery.mobile.defaults.js
@@ -2,18 +2,14 @@
 * start of the application
  */
 $(document).on('mobileinit', function () {
-    console.log('mobileinit');
     $.mobile.defaultDialogTransition = 'none';
     $.mobile.defaultPageTransition = 'none';
 
     GL.cache.groceries.on('pageinit', function () {
-        console.log('groceries pageinit');
-        //GL.init();
         GL.groceries.init();
     });
 
     GL.cache.products.on('pageinit', function () {
-        console.log('products pageinit');
         GL.products.init();
     });
 });
